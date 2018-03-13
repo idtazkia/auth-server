3456548d2a8c912b6dc3241b36b6625ef4d4c8c3
Endy Muhardin <endy.muhardin@gmail.com>
2018-03-13 09:40:26 +0700

    extra dependencies

--- src/main/java/id/ac/tazkia/auth/authserver/AuthServerApplication.java
+++ src/main/java/id/ac/tazkia/auth/authserver/AuthServerApplication.java
@@ -2,9 +2,16 @@ package id.ac.tazkia.auth.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
 public class AuthServerApplication {
+
+	@Bean
+	public SpringDataDialect springDataDialect() {
+		return new SpringDataDialect();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServerApplication.class, args);
