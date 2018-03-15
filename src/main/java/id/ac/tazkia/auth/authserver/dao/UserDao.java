f6594bc8fb278369cdc1de6a5b2f6bd08d876c8f
riski galang saputra <riskigalangsaputra@gmail.com>
2018-03-15 11:27:43 +0700

    crud user

--- src/main/java/id/ac/tazkia/auth/authserver/dao/UserDao.java
+++ src/main/java/id/ac/tazkia/auth/authserver/dao/UserDao.java
@@ -1,10 +1,11 @@
 package id.ac.tazkia.auth.authserver.dao;
 
 import id.ac.tazkia.auth.authserver.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface UserDao extends PagingAndSortingRepository<User, String> {
-    
-    User findByUsername(String username);
-    
+
+    public Page<User> findByusernameContainingIgnoreCase(String username, Pageable page);
 }
