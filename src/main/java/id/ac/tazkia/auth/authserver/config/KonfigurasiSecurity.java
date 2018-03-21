98be937fd323591698ebc64a470699656f888284
Haffizh <haffizh@tazkia.ac.id>
2018-03-21 11:57:17 +0700

    Login

---
+++ src/main/java/id/ac/tazkia/auth/authserver/config/KonfigurasiSecurity.java
@@ -0,0 +1,81 @@
+package id.ac.tazkia.auth.authserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
+    private static final String SQL_LOGIN
+            = "select u.username as username,p.password as password, active\n"
+            + "FROM c_security_user u\n"
+            + "inner join c_security_user_password p on p.id_user = u.id\n"
+            + "WHERE u.username = ?";
+
+    private static final String SQL_PERMISSION
+            = "select u.username, p.permission_value as authority "
+            + "from c_security_user u "
+            + "inner join c_security_role r on u.id_role = r.id "
+            + "inner join c_security_role_permission rp on rp.id_role = r.id "
+            + "inner join c_security_permission p on rp.id_permission = p.id "
+            + "where u.username = ?";
+
+    @Autowired
+    private DataSource ds;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth
+                .jdbcAuthentication()
+                .dataSource(ds)
+                .usersByUsernameQuery(SQL_LOGIN)
+                .authoritiesByUsernameQuery(SQL_PERMISSION)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and().logout().permitAll()
+                .and().formLogin().defaultSuccessUrl("/")
+                .loginPage("/login")
+                .permitAll();
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/favicon.ico")
+                .antMatchers("/info")
+                .antMatchers("/js/*")
+                .antMatchers("/img/**")
+                .antMatchers("/images/*")
+                .antMatchers("/")
+                .antMatchers("/reset-sukses")
+                .antMatchers("/reset")
+                .antMatchers("/404")
+                .antMatchers("/confirm")
+                .antMatchers("/fonts/*")
+                .antMatchers("/css/*");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(17);
+    }
+
+}
--- src/main/java/id/ac/tazkia/auth/authserver/controller/LoginController.java
+++ src/main/java/id/ac/tazkia/auth/authserver/controller/LoginController.java
@@ -1,19 +1,21 @@
 package id.ac.tazkia.auth.authserver.controller;
 
+import id.ac.tazkia.auth.authserver.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- *
- * @author agung
- */
 @Controller
 public class LoginController {
+    @Autowired private UserDao userDao;
 
-    @RequestMapping("/login")
-    public String login(){
-        
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model) {
+        model.addAttribute("loginError", true);
         return "login";
-     }
+    }
+
 
 }
