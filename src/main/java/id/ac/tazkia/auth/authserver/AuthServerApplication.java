854386d8161804301e723992cc78b42148ebdb68
Endy Muhardin <endy.muhardin@gmail.com>
2018-05-18 10:28:31 +0700

    enable authserver

--- src/main/java/id/ac/tazkia/auth/authserver/AuthServerApplication.java
+++ src/main/java/id/ac/tazkia/auth/authserver/AuthServerApplication.java
@@ -3,9 +3,11 @@ package id.ac.tazkia.auth.authserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class AuthServerApplication {
 
     @Bean
